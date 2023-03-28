138d8af274481a1df43399984e9cdcf5768e271f
diego <44531166+daz2712@users.noreply.github.com>
2023-03-28 05:26:32 -0300

    vars

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.60.0"
     }
   }
 }
 
 provider "aws" {
-   region = "us-west-1"
-   profile = "diego"
+  region  = "us-west-1"
+  profile = "diego"
 }
 
 
@@ -17,11 +17,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   # VPC Basic Details
-  name = var.vpc_name
-  cidr = var.vpc_cidr_block
-  azs  = var.vpc_availability_zones
+  name            = var.vpc_name
+  cidr            = var.vpc_cidr_block
+  azs             = var.vpc_availability_zones
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets  
+  private_subnets = var.vpc_private_subnets
 
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = var.vpc_enable_nat_gateway
